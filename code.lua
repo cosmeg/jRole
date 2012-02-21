427165f13316d16a4f66bdc58c342b92c6ecbc0c
John Cramp <john>
2012-02-21 15:31:12 -0800

    fix for 4.3 changes

--- code.lua
+++ code.lua
@@ -8,6 +8,9 @@ UnitSetRole("player","DAMAGER")
 
 -- select dps role when the popup appears
 RolePollPopup:SetScript("OnShow",function()
+	-- try clicking first in case the role is already set
+	RolePollPopupAcceptButton:Click()
+	-- if not, choose dps
 	RolePollPopupRoleButtonDPS:Click()
 	RolePollPopupAcceptButton:Click()
 	UnitSetRole("player","DAMAGER")

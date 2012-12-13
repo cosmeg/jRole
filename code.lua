ed0180c3cee7c68def106db6d93bd8cda23decc7
John Cramp <john>
2012-12-12 18:01:07 -0800

    set role based on spec too

--- code.lua
+++ code.lua
@@ -1,17 +1,33 @@
 -- TODO
 -- set role based on talents (and change on an event)
--- 	although there are some possible downsides of this
+-- 	although there are some possible downsides of this - just print a warning
 
+local function main()
+  -- set role initially (why not)
+  UnitSetRole("player", GetSpecializationRole(GetSpecialization()))
+end
 
--- set role initially (why not)
-UnitSetRole("player","DAMAGER")
-
--- select dps role when the popup appears
+-- select role when the popup appears
 RolePollPopup:SetScript("OnShow",function()
 	-- try clicking first in case the role is already set
 	RolePollPopupAcceptButton:Click()
 	-- if not, choose dps
 	RolePollPopupRoleButtonDPS:Click()
 	RolePollPopupAcceptButton:Click()
-	UnitSetRole("player","DAMAGER")
+
+  -- for tank and heals, we'll choose damage first and then change it after
+  -- TODO fix this
+  local role = GetSpecializationRole(GetSpecialization())
+  if role ~= "DAMAGER" then
+    print("jRole: setting role (based on spec) to "..role)
+  end
+  UnitSetRole("player", role)
 end)
+
+SLASH_JROLE_JROLECHECK1 = "/jrolecheck"
+SLASH_JROLE_JROLECHECK2 = "/jr"
+function SlashCmdList.JROLE_JROLECHECK(msg, editbox)
+  InitiateRolePoll()
+end
+
+main()

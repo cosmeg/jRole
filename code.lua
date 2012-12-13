3a64c3da4aa4e303a3066d356ceca8885696efc2
John Cramp <john>
2012-12-12 18:06:01 -0800

    just hide the window, we're updating role manually below

--- code.lua
+++ code.lua
@@ -1,22 +1,13 @@
--- TODO
--- set role based on talents (and change on an event)
--- 	although there are some possible downsides of this - just print a warning
-
 local function main()
   -- set role initially (why not)
   UnitSetRole("player", GetSpecializationRole(GetSpecialization()))
 end
 
 -- select role when the popup appears
-RolePollPopup:SetScript("OnShow",function()
-	-- try clicking first in case the role is already set
-	RolePollPopupAcceptButton:Click()
-	-- if not, choose dps
-	RolePollPopupRoleButtonDPS:Click()
-	RolePollPopupAcceptButton:Click()
+RolePollPopup:SetScript("OnShow", function(self)
+  -- just hide the window, we're updating role manually below
+  self:Hide()
 
-  -- for tank and heals, we'll choose damage first and then change it after
-  -- TODO fix this
   local role = GetSpecializationRole(GetSpecialization())
   if role ~= "DAMAGER" then
     print("jRole: setting role (based on spec) to "..role)
